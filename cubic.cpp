2ed2a954fbed9ee05857e2a5b300de155820df79
Hemalatha-3 <141707644+Hemalatha-3@users.noreply.github.com>
2023-08-17 11:37:54 +0530

    Create cubic.cpp

---
+++ cubic.cpp
@@ -0,0 +1,15 @@
+#include <math.h>
+
+    double cubic(const double d)
+    {
+        double b = pow(d,3);
+        if (b == b)
+        {
+	   return pow(d,3);
+        }
+
+       else
+       {
+	return 0;
+       }
+   }
